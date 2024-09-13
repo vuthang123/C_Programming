70b58cce37a2d14f37b8882b579b31dab2a2e8c6
ThangVh <hungthang12b2@gmail.com>
2024-09-13 22:49:38 +0700

    update ccmake iar

---
+++ Cmake/day1/build.bat
@@ -0,0 +1,4 @@
+mkdir Step1_build
+cd Step1_build
+cmake -G "MinGW Makefiles" ..
+:: mingw32-make
---
+++ Cmake/day1/main.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+int main()
+{
+    printf("Hello world");
+    return 0;
+}
