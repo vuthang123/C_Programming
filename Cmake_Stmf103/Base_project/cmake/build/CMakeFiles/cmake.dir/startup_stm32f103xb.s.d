b12eb70061be4117e92b5811d477789eacf370f5
DESKTOP-5KECUEV\thang <hungthang12b2@gmail.com>
2025-03-13 20:55:02 +0700

    remove file build

--- Cmake_Stmf103/Base_project/cmake/build/CMakeFiles/cmake.dir/startup_stm32f103xb.s.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\cmake.dir\startup_stm32f103xb.s.obj: \
- C:\Users\THANGVH3\Desktop\C_Programming\Cmake\Base_project\cmake\startup_stm32f103xb.s
--- Cmake_Stmf103/Usart_Lin/cmake/build/CMakeFiles/cmake.dir/startup_stm32f103xb.s.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\cmake.dir\startup_stm32f103xb.s.obj: \
- C:\Users\thang\Desktop\C_Programming\Cmake_Stmf103\Usart_Lin\cmake\startup_stm32f103xb.s
